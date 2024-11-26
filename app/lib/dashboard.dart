9ef6f8ff70f65019c34bffd2539c5ecda53fdaba
Raja Rai <raja.rai@silvergrey.in>
2024-11-26 13:38:58 +0530

    new Changes scrolling effect

--- app/lib/dashboard.dart
+++ app/lib/dashboard.dart
@@ -30,11 +30,16 @@ class _DashboardPageState extends State<DashboardPage> {
 
   void _startAutoScroll() {
     _timer = Timer.periodic(const Duration(seconds: 3), (Timer timer) {
-      if (_currentPage < backgroundImages.length - 1) {
-        _currentPage++;
-      } else {
-        _currentPage = 0;
-      }
+      // Update the current page index
+      setState(() {
+        if (_currentPage < backgroundImages.length - 1) {
+          _currentPage++;
+        } else {
+          _currentPage = 0;
+        }
+      });
+
+      // Animate to the new page
       _pageController.animateToPage(
         _currentPage,
         duration: const Duration(milliseconds: 800),
@@ -53,173 +58,66 @@ class _DashboardPageState extends State<DashboardPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Stack(
-      children: [
-        SingleChildScrollView(
-          child: Column(
+      body: Stack(
+        children: [
+          Column(
             children: [
               Container(
                 padding: EdgeInsets.symmetric(horizontal: 15, vertical: 15),
                 color: AppTheme.black,
                 child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Image.asset(
-                        "assets/images/Taxi-Logo.png",
-                        width: 150,
-                      ),
-                      TextButton(
-                          style: ButtonStyle(
-                            backgroundColor:
-                                MaterialStateProperty.all(Colors.green),
-                            shape: MaterialStateProperty.all(
-                              RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(12),
-                              ),
-                            ),
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Image.asset(
+                      "assets/images/Taxi-Logo.png",
+                      width: 150,
+                    ),
+                    TextButton(
+                      style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all(Colors.green),
+                        shape: MaterialStateProperty.all(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(12),
                           ),
-                          onPressed: () {},
-                          child: Padding(
-                            padding: EdgeInsets.symmetric(
-                                horizontal: 10, vertical: 5),
-                            child: Text(
-                              'BOOK NOW',
-                              style: TextStyle(
-                                  fontSize: 12, color: AppTheme.white),
-                            ),
-                          ))
-                    ]),
+                        ),
+                      ),
+                      onPressed: () {},
+                      child: Padding(
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                        child: Text(
+                          'BOOK NOW',
+                          style: TextStyle(fontSize: 12, color: AppTheme.white),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
               ),
-              PageView.builder(
-                controller: _pageController,
-                itemCount: backgroundImages.length,
-                onPageChanged: (int page) {
-                  setState(() {
-                    _currentPage = page;
-                  });
-                },
-                itemBuilder: (context, index) {
-                  return Image.asset(
-                    backgroundImages[index],
-                    fit: BoxFit.cover,
-                  );
-                },
+              // PageView without unnecessary scroll wrapping
+              SizedBox(
+                height: 400, // You can adjust the height as needed
+                child: PageView.builder(
+                  controller: _pageController,
+                  itemCount: backgroundImages.length,
+                  onPageChanged: (int page) {
+                    setState(() {
+                      _currentPage = page;
+                    });
+                  },
+                  itemBuilder: (context, index) {
+                    return Image.asset(
+                      backgroundImages[index],
+                      fit: BoxFit.cover,
+                    );
+                  },
+                ),
               ),
             ],
           ),
-        )
-      ],
-    ));
+        ],
+      ),
+    );
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-// class DashboardPage extends StatefulWidget {
-//   const DashboardPage({super.key});
-
-//   @override
-//   State<DashboardPage> createState() => _DashboardPageState();
-// }
-
-// class _DashboardPageState extends State<DashboardPage> {
-//   final PageController _pageController = PageController();
-//   int _currentPage = 0;
-//   Timer? _timer;
-
-//   final List<String> backgroundImages = [
-//     'assets/images/women.png',
-//     'assets/images/girl.png',
-//     'assets/images/redwomen.png',
-//     'assets/images/mathura.png',
-//   ];
-
-//   @override
-//   void initState() {
-//     super.initState();
-//     _startAutoScroll();
-//   }
-
-//   void _startAutoScroll() {
-//     _timer = Timer.periodic(const Duration(seconds: 3), (Timer timer) {
-//       if (_currentPage < backgroundImages.length - 1) {
-//         _currentPage++;
-//       } else {
-//         _currentPage = 0;
-//       }
-//       _pageController.animateToPage(
-//         _currentPage,
-//         duration: const Duration(milliseconds: 800),
-//         curve: Curves.easeInOut,
-//       );
-//     });
-//   }
-
-//   @override
-//   void dispose() {
-//     _timer?.cancel();
-//     _pageController.dispose();
-//     super.dispose();
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//         body: Stack(
-//       children: [
-//         Container(
-//           padding: EdgeInsets.symmetric(horizontal: 15, vertical: 15),
-//           color: AppTheme.black,
-//           child:
-//               Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-//             Image.asset(
-//               "assets/images/Taxi-Logo.png",
-//               width: 150,
-//             ),
-//             TextButton(
-//                 style: ButtonStyle(
-//                   backgroundColor: MaterialStateProperty.all(Colors.green),
-//                   shape: MaterialStateProperty.all(
-//                     RoundedRectangleBorder(
-//                       borderRadius: BorderRadius.circular(12),
-//                     ),
-//                   ),
-//                 ),
-//                 onPressed: () {},
-//                 child: Padding(
-//                   padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-//                   child: Text(
-//                     'BOOK NOW',
-//                     style: TextStyle(fontSize: 12, color: AppTheme.white),
-//                   ),
-//                 ))
-//           ]),
-//         ),
-//         PageView.builder(
-//           controller: _pageController,
-//           itemCount: backgroundImages.length,
-//           onPageChanged: (int page) {
-//             setState(() {
-//               _currentPage = page;
-//             });
-//           },
-//           itemBuilder: (context, index) {
-//             return Image.asset(
-//               backgroundImages[index],
-//               fit: BoxFit.cover,
-//             );
-//           },
-//         ),
-//       ],
-//     ));
-//   }
-// }
